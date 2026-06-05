3dffea1a3c0a7b9aab1ee981bc0293b856b7105b
Nadezda-Gus <nadezdagusarjva393@gmail.com>
2026-06-05 19:48:22 +0700

    .

--- chat-app/src/utils/supabase.js
+++ chat-app/src/utils/supabase.js
@@ -1,5 +1,5 @@
 import { createClient } from '@supabase/supabase-js'; // создание клиента Supabase
-import config from '../config/config.js'; // импорт конфига
+import config from '../config.js'; // импорт конфига
 
 // Создаём клиент Supabase с URL и анонимным ключом из конфига
 const supabase = createClient(config.supabase.url, config.supabase.anonKey);
